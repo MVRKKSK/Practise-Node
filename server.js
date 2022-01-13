4e03325f70e3c963d2f21302e024cf1f0c21bf98
MVRKKSK <miryala.kautilya2020@vitbhopal.ac.in>
2022-01-13 12:33:08 +0530

    created routing using nodejs

--- server.js
+++ server.js
@@ -1,9 +1,34 @@
 const http = require("http");
+const fs = require("fs")
 
 const server = http.createServer((req, res) => {
-    console.log("server is running");
+
+    let path = "./views/";
+    switch (req.url) {
+        case "/":
+            path += "index.html";
+            break;
+        case "/about":
+            path += "about.html";
+            break;
+        case "/404":
+            path += "404.html";
+            break;
+    }
+
+    fs.readFile(path, ((err, data) => {
+        if (err) {
+            console.log(err);
+            res.end()
+        }
+        else {
+            res.write(data);
+            res.end();
+        }
+
+    }))
 });
 
-server.listen(3000 , () => {
+server.listen(3000, () => {
     console.log("this server is running at port 3000")
 })
